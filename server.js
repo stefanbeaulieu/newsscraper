c3353b38ae050fec607d7a9c06a87e25ee0cd120
Stefan Beaulieu <beaulieustefan@gmail.com>
2017-03-27 17:51:48 -0400

    Pages set up

---
+++ server.js
@@ -0,0 +1,51 @@
+// Require Dependencies
+var express = require('express');
+var expressHandlebars = require("express-handlebars");
+var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+
+// Set up ports to be either hosts or designated port
+var PORT = process.env.PORT || 3000;
+
+// Set up express app
+var app = express();
+
+// Setting up the express router
+var router = express.Router();
+
+// Designate our public folder as a static directory
+app.use(express.static(__dirname + "/public"));
+
+// Connect Handlebars to our Express app
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set('view engine', "handlebards");
+
+// use bodyParser in our app
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+// Have very request go through our router middleware
+app.use(router);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Connect to mongoose for our database
+mongoose.connect(db, function (error) {
+    // Log any errors connecting with mongoodse
+    if ( error ) {
+        console.log(error);
+    }
+    // Or log a success message
+    else {
+        console.log ('mongoose connection is successful');
+    }
+});
+
+// Listen to the port
+app.listen(PORT, function () {
+    console.log("Listening on PORT: " + PORT);
+})
